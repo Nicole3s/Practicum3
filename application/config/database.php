b1eef67a7a7f1d1f8cc0088a5bf9225d3c6d63c6
Tijmen <tijmenvandenpol@hotmail.com>
2016-04-06 16:17:27 +0200

    tutorial
    routing enzo

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group = 'tijmen';
 $query_builder = TRUE;
 
 $db['default'] = array(
@@ -93,4 +93,26 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
+);
+
+$db['tijmen'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost:8000',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'database_name',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => TRUE,
+	'db_debug' => TRUE,
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array()//,
+//	'save_queries' => TRUE
 );
